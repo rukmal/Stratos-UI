d03cd4ba2691bbe5601acf53591cf0804c55de35
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-08-07 15:32:16 +0530

    Finished popup menu

--- js/form.js
+++ js/form.js
@@ -18,9 +18,30 @@ function changeBoxHeight () {
 $('#requests, #memory, #loadaverage').on('hidden.bs.collapse', changeBoxHeight);
 $('#requests, #memory, #loadaverage').on('shown.bs.collapse', changeBoxHeight);
 
+// menu variables
+var menuIsShown = false;
+
 /**
  * Function to handle bringing the side menu out
  */
 $('.menutoggle').click(function () {
-	console.log('here');
+	if (menuIsShown) {
+		// take menu in
+		$('.menucontent').animate({
+			'left': '-10em'
+		}, 500);
+		$('.menutoggle').animate({
+			'left': '-=7.3em'
+		}, 500);
+		menuIsShown = false;
+	} else {
+		// bring menu out
+		$('.menucontent').animate({
+			'left': '0em'
+		}, 500);
+		$('.menutoggle').animate({
+			'left': '+=7.3em'
+		}, 500);
+		menuIsShown = true;
+	}
 });
