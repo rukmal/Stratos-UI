1b3fac0df8a809bc626c6ac5e7950fdbead1e215
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-08-26 14:06:04 +0530

    Removed everything. Switching to Jaggery

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-// Setting the height of the centered div, and adding the centering class
-var actualHeight = $('#centered').height();
-
-$('#centered').css('height', actualHeight);
-$('#centered').addClass('vertical-center');
